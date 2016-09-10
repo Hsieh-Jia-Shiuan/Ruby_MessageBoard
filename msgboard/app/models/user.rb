b429939e68ba53fe4e0340205d545daefa9030c4
Hsieh-Jia-Shiuan <w96652847@gmail.com>
2016-09-10 18:20:34 +0800

    add user authenication & user controller & sessions controller

--- msgboard/app/models/user.rb
+++ msgboard/app/models/user.rb
@@ -3,8 +3,10 @@ class User < ActiveRecord::Base
 	has_many :posts
 
 	validates :name, presence: true
-	validates :name, presence: true
+	validates :email, presence: true
 
 	validates :name, length: {minimum: 2}
 	validates :name, length: {maximum: 100}
+
+	has_secure_password validations: false
 end

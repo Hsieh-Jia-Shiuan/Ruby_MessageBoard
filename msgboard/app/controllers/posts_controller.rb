bf0dc072210c7c83b7d54177141fbfbe0097c244
Hsieh-Jia-Shiuan <w96652847@gmail.com>
2016-09-17 19:42:09 +0800

    setting users when creating new post and comment & added dropdown menu into navigaion bar

--- msgboard/app/controllers/posts_controller.rb
+++ msgboard/app/controllers/posts_controller.rb
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
   def create
     #前端使用者傳回的資料，建立一個新的 post 物件
     post = Post.new(post_params)
+    post.user = current_user
 
     #如果成功存進資料庫，就導回 index 頁面，失敗就再顯示一次表單
     if post.save!
--- msgboard/app/models/user.rb
+++ msgboard/app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
 	has_many :comments
 	has_many :posts
 
-	validates :name, presence: true
-	validates :password_digest, presence: true
+	validates :name, presence: true, uniqueness: true
+	validates :password_digest, presence: true, on: :create, length: {minimum: 5}
 
 	validates :name, length: {minimum: 2}
 	validates :name, length: {maximum: 100}
